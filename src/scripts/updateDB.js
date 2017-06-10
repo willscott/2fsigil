fad4715d92c0ec84525674b7585d72ff8684a977
Will Scott <will@cypherpunk.email>
2017-06-10 17:55:07 -0400

    callback for when refresh complete

--- src/scripts/updateDB.js
+++ src/scripts/updateDB.js
@@ -18,23 +18,35 @@ var refresh = function(name, cb) {
   xhr.send();
 }
 
-var GetFiles = function() {
+var GetFiles = function(cb) {
   refresh("", function(dat) {
     var listing = JSON.parse(dat);
+    var state = {inprogress: 0, files: [], done: cb};
+    state.complete = (function () {
+      this.inprogress--;
+      if (this.inprogress == 0) {
+        this.done();
+      }
+    }).bind(state);
     listing.forEach(function (file) {
-      GetFile(file.name, file.sha);
+      state.inprogress++;
+      state.files.push(file.name);
+      GetFile(file.name, file.sha, state);
     });
   })
 }
 
-var GetFile = function(name, hash) {
+var GetFile = function(name, hash, state) {
   storage.get('file/' + name, function(dat) {
     if(dat.hash !== hash) {
       refresh(name, (function(name, hash, dat) {
         var doc = yaml.safeLoad(dat);
         doc.hash = hash;
         storage.set({'file/' + name : doc});
+        state.complete();
       }).bind(this, name, hash));
+    } else {
+      state.complete();
     }
   });
 };
